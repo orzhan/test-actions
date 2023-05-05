c63fac6cd0ae71b93e7a02729184a7654ecdb0c6
Mikhail Orzhenovsky <mikhail.orzhenovsky@etnatrader.com>
2023-05-05 17:48:57 +0300

    fix

--- fact_check.py
+++ fact_check.py
@@ -2,6 +2,7 @@ import os
 import sys
 import requests
 from github import Github, GithubException
+import subprocess
 
 # Get the arguments passed to the script
 repo_url = sys.argv[1]
@@ -15,21 +16,41 @@ token = os.environ.get("GITHUB_TOKEN")
 # Initialize the Github object
 g = Github(token)
 
+repo_name = "/".join(repo_url.split("/")[-2:]).replace(".git", "")
+
 # Get the repository object for the pull request
-repo = g.get_repo("/".join(repo_url.split("/")[-2:]))
+repo = g.get_repo(repo_name)
 
 # Get the pull request object
 pull_request = repo.get_pull(pull_request_number)
 
 # Get the diff for the pull request
-diff_url = f"{repo_url}/compare/{base_branch}...{head_branch}.diff"
-diff_response = requests.get(diff_url)
+#diff_url = f"{repo_url}/compare/{base_branch}...{head_branch}.diff"
+#diff_response = requests.get(diff_url)
+
+# Get the repository path on the runner
+repo_path = os.path.join(os.environ['GITHUB_WORKSPACE'], repo.full_name)
+print('repo_path', repo_path)
+
+# Check out the base branch and head branch
+subprocess.run(['git', 'checkout', base_branch], cwd=repo_path, check=True)
+subprocess.run(['git', 'checkout', head_branch], cwd=repo_path, check=True)
+
+# Get the diff for the pull request
+diff_output = subprocess.check_output(['git', 'diff', '--no-prefix', '--unified=0', base_branch, head_branch], cwd=repo_path)
+
+# Convert the bytes to a string
+diff_str = diff_output.decode('utf-8')
+
+# Print the diff
+print(diff_str)
+
 
 # Parse the diff and check for made-up lines
 # Replace the following code with your fact-checking logic
 
 made_up_lines = []
-for line in diff_response.text.splitlines():
+for line in diff_str.splitlines():
     if line.startswith("+") and "made up" in line:
         made_up_lines.append(line)
 
